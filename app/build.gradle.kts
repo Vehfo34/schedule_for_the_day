168888f65b4f731fcf979eb398f8cf440801b531
Vehfo34 <vehfozalivakin@mail.ru>
2026-07-22 14:04:02 +0300

    1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 }
 android {
     namespace = "com.example.schedule_for_the_day"
-    compileSdk = 37 // Стабильная версия
+    compileSdk = 36 // Стабильная версия
 
     defaultConfig {
         applicationId = "com.example.schedule_for_the_day"
         minSdk = 24
-        targetSdk = 36
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
@@ -41,8 +41,10 @@ android {
 
 dependencies {
     implementation(platform(libs.androidx.compose.bom))
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.lifecycle.runtime.ktx)
+    // Заменяем core-ktx на явную версию 1.13.0
+    implementation("androidx.core:core-ktx:1.13.0")    // вместо libs.androidx.core.ktx
+    // Если lifecycle-runtime-ktx тоже из libs и имеет версию 2.11.0, лучше явно указать 2.8.0:
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.0")
     implementation(libs.androidx.activity.compose)
     implementation(libs.androidx.compose.ui)
     implementation(libs.androidx.compose.ui.graphics)
@@ -52,21 +54,16 @@ dependencies {
     implementation("androidx.room:room-ktx:2.8.4")
     ksp("androidx.room:room-compiler:2.8.4")
     implementation("androidx.datastore:datastore-preferences:1.2.1")
-
-
-
     implementation("androidx.navigation:navigation-compose:2.9.8")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.11.0")
+    // Понижаем lifecycle-viewmodel-compose
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.0") // вместо 2.11.0
     implementation("androidx.compose.material:material-icons-extended")
-
 
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(platform(libs.androidx.compose.bom))
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
-
-
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
 }
